695560fd44c3bfa08110a0a49510ac0d44cba1ce
nel-fura <neluba.artem1@gmail.com>
2026-06-04 21:34:42 +0300

    lab19

---
+++ lab19/lab19/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace lab19
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.codeBox = new System.Windows.Forms.RichTextBox();
+            this.correctCodeButton = new System.Windows.Forms.Button();
+            this.incorrectCodeButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkButton
+            // 
+            this.checkButton.Location = new System.Drawing.Point(401, 13);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(226, 35);
+            this.checkButton.TabIndex = 1;
+            this.checkButton.Text = "Check";
+            this.checkButton.UseVisualStyleBackColor = true;
+            this.checkButton.Click += new System.EventHandler(this.checkButton_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(6, 16);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 13);
+            this.resultLabel.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.resultLabel);
+            this.groupBox1.Location = new System.Drawing.Point(401, 54);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(226, 289);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Results";
+            // 
+            // codeBox
+            // 
+            this.codeBox.Location = new System.Drawing.Point(13, 13);
+            this.codeBox.Name = "codeBox";
+            this.codeBox.Size = new System.Drawing.Size(382, 425);
+            this.codeBox.TabIndex = 4;
+            this.codeBox.Text = "";
+            // 
+            // correctCodeButton
+            // 
+            this.correctCodeButton.Location = new System.Drawing.Point(9, 19);
+            this.correctCodeButton.Name = "correctCodeButton";
+            this.correctCodeButton.Size = new System.Drawing.Size(211, 25);
+            this.correctCodeButton.TabIndex = 5;
+            this.correctCodeButton.Text = "Correct Code";
+            this.correctCodeButton.UseVisualStyleBackColor = true;
+            this.correctCodeButton.Click += new System.EventHandler(this.correctCodeButton_Click);
+            // 
+            // incorrectCodeButton
+            // 
+            this.incorrectCodeButton.Location = new System.Drawing.Point(9, 50);
+            this.incorrectCodeButton.Name = "incorrectCodeButton";
+            this.incorrectCodeButton.Size = new System.Drawing.Size(211, 25);
+            this.incorrectCodeButton.TabIndex = 6;
+            this.incorrectCodeButton.Text = "Incorrect Code";
+            this.incorrectCodeButton.UseVisualStyleBackColor = true;
+            this.incorrectCodeButton.Click += new System.EventHandler(this.incorrectCodeButton_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.correctCodeButton);
+            this.groupBox2.Controls.Add(this.incorrectCodeButton);
+            this.groupBox2.Location = new System.Drawing.Point(401, 349);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(226, 89);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Testing Templates";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.codeBox);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.checkButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Lab 19 Var 16 - testing java bracket syntaxt";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button checkButton;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox codeBox;
+        private System.Windows.Forms.Button correctCodeButton;
+        private System.Windows.Forms.Button incorrectCodeButton;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ lab19/lab19/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab19
+{
+    public partial class Form1 : Form
+    {
+
+        string wrongCode = @"function greet(name {
+            if (name.length > 0) {
+                console.log(""Hello, "" + name);
+
+                for (let i = 0; i < 3; i++) {
+                    console.log(i);
+                ]
+
+            } else {
+                console.log(""No name provided"");
+            }
+
+        }
+
+        greet(""Alice"");
+        ";
+
+        string rightCode = @"function greet(name) {
+            if (name.length > 0) {
+                console.log(""Hello, "" + name);
+
+                for (let i = 0; i < 3; i++) {
+                   console.log(i);
+                }   
+             } else {
+                console.log(""No name provided"");
+             }
+        }
+
+        greet(""Alice"");
+        ";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            string expression = codeBox.Text;
+
+            if (string.IsNullOrWhiteSpace(expression))
+            {
+                resultLabel.Text = "Enter an expression to check.";
+                resultLabel.ForeColor = Color.Gray;
+                return;
+            }
+
+            var opens = new Dictionary<char, char>
+            {
+                { '(', ')' },
+                { '[', ']' },
+                { '{', '}' }
+            };
+
+            var closes = new HashSet<char> { ')', ']', '}' };
+            var stack = new Stack<(char ch, int index)>();
+            string error = null;
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                char ch = expression[i];
+
+                if (opens.ContainsKey(ch))
+                {
+                    stack.Push((ch, i));
+                }
+                else if (closes.Contains(ch))
+                {
+                    if (stack.Count == 0)
+                    {
+                        error = $"Extra closing bracket '{ch}' at position {i + 1}";
+                        break;
+                    }
+
+                    var (top, _) = stack.Pop();
+
+                    if (opens[top] != ch)
+                    {
+                        error = $"Expected '{opens[top]}' but found '{ch}' at position {i + 1}";
+                        break;
+                    }
+                }
+            }
+
+            if (error == null && stack.Count > 0)
+            {
+                var (unclosed, pos) = stack.Peek();
+                error = $"Unclosed bracket '{unclosed}' at position {pos + 1}";
+            }
+
+            if (error == null)
+            {
+                resultLabel.ForeColor = Color.Green;
+                resultLabel.Text = "Brackets are valid.";
+            }
+            else
+            {
+                resultLabel.ForeColor = Color.Red;
+                resultLabel.Text = error;
+            }
+        }
+
+        private void correctCodeButton_Click(object sender, EventArgs e)
+        {
+            codeBox.Text = rightCode;
+        }
+
+        private void incorrectCodeButton_Click(object sender, EventArgs e)
+        {
+            codeBox.Text = wrongCode;
+
+        }
+    }
+}
