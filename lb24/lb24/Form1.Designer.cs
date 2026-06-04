c2b1c336ff09488be4c4f552b19d8cd5a91b1a98
nel-fura <neluba.artem1@gmail.com>
2026-06-04 23:50:13 +0300

    Add files via upload

---
+++ lb24/lb24/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace lb24
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.buttonStart1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.buttonStart2 = new System.Windows.Forms.Button();
+            this.buttonStart3 = new System.Windows.Forms.Button();
+            this.buttonStartAll = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(254, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(274, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(254, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(534, 12);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(254, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // buttonStart1
+            // 
+            this.buttonStart1.Location = new System.Drawing.Point(12, 295);
+            this.buttonStart1.Name = "buttonStart1";
+            this.buttonStart1.Size = new System.Drawing.Size(254, 29);
+            this.buttonStart1.TabIndex = 3;
+            this.buttonStart1.Text = "Запустити 1";
+            this.buttonStart1.UseVisualStyleBackColor = true;
+            this.buttonStart1.Click += new System.EventHandler(this.buttonStart1_Click_1);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 38);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(254, 251);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(274, 38);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(254, 251);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(534, 38);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(254, 251);
+            this.richTextBox3.TabIndex = 6;
+            this.richTextBox3.Text = "";
+            // 
+            // buttonStart2
+            // 
+            this.buttonStart2.Location = new System.Drawing.Point(274, 295);
+            this.buttonStart2.Name = "buttonStart2";
+            this.buttonStart2.Size = new System.Drawing.Size(254, 29);
+            this.buttonStart2.TabIndex = 3;
+            this.buttonStart2.Text = "Запустити 2";
+            this.buttonStart2.UseVisualStyleBackColor = true;
+            this.buttonStart2.Click += new System.EventHandler(this.buttonStart2_Click_1);
+            // 
+            // buttonStart3
+            // 
+            this.buttonStart3.Location = new System.Drawing.Point(534, 295);
+            this.buttonStart3.Name = "buttonStart3";
+            this.buttonStart3.Size = new System.Drawing.Size(254, 29);
+            this.buttonStart3.TabIndex = 3;
+            this.buttonStart3.Text = "Запустити 3";
+            this.buttonStart3.UseVisualStyleBackColor = true;
+            this.buttonStart3.Click += new System.EventHandler(this.buttonStart3_Click_1);
+            // 
+            // buttonStartAll
+            // 
+            this.buttonStartAll.Location = new System.Drawing.Point(12, 330);
+            this.buttonStartAll.Name = "buttonStartAll";
+            this.buttonStartAll.Size = new System.Drawing.Size(776, 29);
+            this.buttonStartAll.TabIndex = 7;
+            this.buttonStartAll.Text = "Запустити всі потоки";
+            this.buttonStartAll.UseVisualStyleBackColor = true;
+            this.buttonStartAll.Click += new System.EventHandler(this.buttonStartAll_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 367);
+            this.Controls.Add(this.buttonStartAll);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.buttonStart3);
+            this.Controls.Add(this.buttonStart2);
+            this.Controls.Add(this.buttonStart1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "lab24 var16";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button buttonStart1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button buttonStart2;
+        private System.Windows.Forms.Button buttonStart3;
+        private System.Windows.Forms.Button buttonStartAll;
+    }
+}
+
---
+++ lb24/lb24/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lb24
+{
+    public partial class Form1 : Form
+    {
+        Thread thread1, thread2, thread3;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormClosed += Form1_FormClosed;
+        }
+        void SafeStart(ref Thread t, ThreadStart method)
+        {
+            if (t == null || !t.IsAlive)
+            {
+                t = new Thread(method) { IsBackground = true };
+                t.Start();
+            }
+        }
+
+        //CAST 
+        void RunCAST()
+        {
+            try
+            {
+                string input = "";
+                richTextBox1.Invoke((MethodInvoker)(() => input = textBox1.Text));
+                if (string.IsNullOrEmpty(input)) input = "Hello CAST";
+
+                byte[] key = Encoding.UTF8.GetBytes("CAST16KEY12345AB");
+                byte[] data = Encoding.UTF8.GetBytes(input);
+                byte[] result = CastEncrypt(data, key);
+                string hex = BitConverter.ToString(result).Replace("-", " ");
+
+                richTextBox1.Invoke((MethodInvoker)(() =>
+                {
+                    richTextBox1.Clear();
+                    richTextBox1.AppendText("CAST\n");
+                    richTextBox1.AppendText($"Вхід: {input}\n");
+                    richTextBox1.AppendText($"Зашифровано:\n{hex}\n");
+                }));
+            }
+            catch (ThreadAbortException) { }
+            catch (Exception ex)
+            {
+                richTextBox1.Invoke((MethodInvoker)(() =>
+                    richTextBox1.AppendText("Помилка: " + ex.Message)));
+            }
+        }
+
+        byte[] CastEncrypt(byte[] data, byte[] key)
+        {
+            byte[] result = new byte[data.Length];
+            for (int i = 0; i < data.Length; i++)
+                result[i] = (byte)(data[i] ^ key[i % key.Length] ^ (byte)(i * 3 + 7));
+            return result;
+        }
+
+        //MD-4
+        void RunMD4()
+        {
+            try
+            {
+                string input = "";
+                richTextBox2.Invoke((MethodInvoker)(() => input = textBox2.Text));
+                if (string.IsNullOrEmpty(input)) input = "Hello MD4";
+
+                byte[] hash = MD4Hash(Encoding.UTF8.GetBytes(input));
+                string hashHex = BitConverter.ToString(hash).Replace("-", "").ToLower();
+
+                richTextBox2.Invoke((MethodInvoker)(() =>
+                {
+                    richTextBox2.Clear();
+                    richTextBox2.AppendText("MD-4\n");
+                    richTextBox2.AppendText($"Вхід: {input}\n");
+                    richTextBox2.AppendText($"Хеш:\n{hashHex}\n");
+                }));
+            }
+            catch (ThreadAbortException) { }
+            catch (Exception ex)
+            {
+                richTextBox2.Invoke((MethodInvoker)(() =>
+                    richTextBox2.AppendText("Помилка: " + ex.Message)));
+            }
+        }
+
+        byte[] MD4Hash(byte[] input)
+        {
+            uint A = 0x67452301, B = 0xEFCDAB89, C = 0x98BADCFE, D = 0x10325476;
+            int origLen = input.Length;
+            int padLen = ((origLen % 64) < 56) ? (56 - origLen % 64) : (120 - origLen % 64);
+            byte[] msg = new byte[origLen + padLen + 8];
+            Array.Copy(input, msg, origLen);
+            msg[origLen] = 0x80;
+            BitConverter.GetBytes((ulong)(origLen * 8)).CopyTo(msg, msg.Length - 8);
+
+            for (int i = 0; i < msg.Length / 64; i++)
+            {
+                uint[] X = new uint[16];
+                for (int j = 0; j < 16; j++)
+                    X[j] = BitConverter.ToUInt32(msg, i * 64 + j * 4);
+
+                uint a = A, b = B, c = C, d = D;
+
+                uint[] r1 = { 3, 7, 11, 19 };
+                for (int j = 0; j < 16; j++)
+                {
+                    uint F = (b & c) | (~b & d);
+                    uint tmp = a + F + X[j];
+                    int s = (int)r1[j % 4];
+                    a = d; d = c; c = b;
+                    b = (tmp << s) | (tmp >> (32 - s));
+                }
+
+                uint[] r2 = { 3, 5, 9, 13 };
+                uint[] idx2 = { 0, 4, 8, 12, 1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15 };
+                for (int j = 0; j < 16; j++)
+                {
+                    uint G = (b & c) | (b & d) | (c & d);
+                    uint tmp = a + G + X[idx2[j]] + 0x5A827999;
+                    int s = (int)r2[j % 4];
+                    a = d; d = c; c = b;
+                    b = (tmp << s) | (tmp >> (32 - s));
+                }
+
+                uint[] r3 = { 3, 9, 11, 15 };
+                uint[] idx3 = { 0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15 };
+                for (int j = 0; j < 16; j++)
+                {
+                    uint H = b ^ c ^ d;
+                    uint tmp = a + H + X[idx3[j]] + 0x6ED9EBA1;
+                    int s = (int)r3[j % 4];
+                    a = d; d = c; c = b;
+                    b = (tmp << s) | (tmp >> (32 - s));
+                }
+
+                A += a; B += b; C += c; D += d;
+            }
+
+            byte[] result = new byte[16];
+            BitConverter.GetBytes(A).CopyTo(result, 0);
+            BitConverter.GetBytes(B).CopyTo(result, 4);
+            BitConverter.GetBytes(C).CopyTo(result, 8);
+            BitConverter.GetBytes(D).CopyTo(result, 12);
+            return result;
+        }
+
+        //SEAL 
+        void RunSEAL()
+        {
+            try
+            {
+                string input = "";
+                richTextBox3.Invoke((MethodInvoker)(() => input = textBox3.Text));
+                if (string.IsNullOrEmpty(input)) input = "Hello SEAL";
+
+                byte[] data = Encoding.UTF8.GetBytes(input);
+                byte[] key = Encoding.UTF8.GetBytes("SEALKEY1");
+                byte[] encrypted = SealEncrypt(data, key);
+                string hex = BitConverter.ToString(encrypted).Replace("-", " ");
+
+                richTextBox3.Invoke((MethodInvoker)(() =>
+                {
+                    richTextBox3.Clear();
+                    richTextBox3.AppendText("SEAL\n");
+                    richTextBox3.AppendText($"Вхід: {input}\n");
+                    richTextBox3.AppendText($"Зашифровано:\n{hex}\n");
+                }));
+            }
+            catch (ThreadAbortException) { }
+            catch (Exception ex)
+            {
+                richTextBox3.Invoke((MethodInvoker)(() =>
+                    richTextBox3.AppendText("Помилка: " + ex.Message)));
+            }
+        }
+
+        private void buttonStart1_Click_1(object sender, EventArgs e)
+        {
+            SafeStart(ref thread1, RunCAST);
+        }
+
+        private void buttonStart2_Click_1(object sender, EventArgs e)
+        {
+            SafeStart(ref thread2, RunMD4);
+        }
+
+        private void buttonStart3_Click_1(object sender, EventArgs e)
+        {
+            SafeStart(ref thread3, RunSEAL);
+        }
+
+        private void buttonStartAll_Click(object sender, EventArgs e)
+        {
+            SafeStart(ref thread1, RunCAST);
+            SafeStart(ref thread2, RunMD4);
+            SafeStart(ref thread3, RunSEAL);
+        }
+
+        byte[] SealEncrypt(byte[] data, byte[] key)
+        {
+            byte[] result = new byte[data.Length];
+            uint state = 0;
+            for (int i = 0; i < key.Length; i++)
+                state = (state << 8) | key[i];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                state ^= state << 13;
+                state ^= state >> 17;
+                state ^= state << 5;
+                result[i] = (byte)(data[i] ^ (state & 0xFF));
+            }
+            return result;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            thread1?.Abort();
+            thread2?.Abort();
+            thread3?.Abort();
+        }
+    }
+}
