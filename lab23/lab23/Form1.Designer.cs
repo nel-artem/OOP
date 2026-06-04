033d7f00072c2ac4fe7b560c9614d1ba67eda66c
nel-fura <neluba.artem1@gmail.com>
2026-06-04 22:41:17 +0300

    Add files via upload

---
+++ lab23/lab23/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace lab23
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxR = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_r = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(431, 403);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox_r);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBoxR);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 444);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(443, 48);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "R:";
+            // 
+            // textBoxR
+            // 
+            this.textBoxR.Location = new System.Drawing.Point(31, 17);
+            this.textBoxR.Name = "textBoxR";
+            this.textBoxR.Size = new System.Drawing.Size(100, 20);
+            this.textBoxR.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(137, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "r:";
+            // 
+            // textBox_r
+            // 
+            this.textBox_r.Location = new System.Drawing.Point(156, 17);
+            this.textBox_r.Name = "textBox_r";
+            this.textBox_r.Size = new System.Drawing.Size(100, 20);
+            this.textBox_r.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(262, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 28);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Build";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(443, 426);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Graph";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 500);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "lab23 var16";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox_r;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxR;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ lab23/lab23/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace lab23
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawHypocycloid(double R, double r, PictureBox canvas)
+        {
+            int width = canvas.Width;
+            int height = canvas.Height;
+
+            Bitmap bmp = new Bitmap(width, height);
+            Graphics g = Graphics.FromImage(bmp);
+            g.Clear(Color.White);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            // Центр координат
+            int cx = width / 2;
+            int cy = height / 2;
+
+            double m = r / R;
+
+            // Масштаб — щоб крива вміщалась у PictureBox
+            double scale = (Math.Min(width, height) / 2.0) / R * 0.85;
+
+            DrawAxes(g, cx, cy, width, height);
+
+            // Малюємо криву
+            Pen curvePen = new Pen(Color.Blue, 2);
+            double step = 0.001;
+            double tMax = 2 * Math.PI * GetPeriods(r, R);
+
+            double? prevX = null, prevY = null;
+
+            for (double t = 0; t <= tMax; t += step)
+            {
+                double x = (R - m * R) * Math.Cos(m * t) + m * Math.Cos(t - m * t);
+                double y = (R - m * R) * Math.Sin(m * t) - m * Math.Sin(t - m * t);
+
+                double screenX = cx + x * scale;
+                double screenY = cy - y * scale;
+
+                if (prevX.HasValue)
+                {
+                    g.DrawLine(curvePen,
+                        (float)prevX.Value, (float)prevY.Value,
+                        (float)screenX, (float)screenY);
+                }
+
+                prevX = screenX;
+                prevY = screenY;
+            }
+
+            canvas.Image = bmp;
+
+            g.Dispose();
+            curvePen.Dispose();
+        }
+
+        private void DrawAxes(Graphics g, int cx, int cy, int width, int height)
+        {
+            Pen axisPen = new Pen(Color.Black, 1.5f);
+            Font font = new Font("Arial", 8);
+            Brush brush = Brushes.Black;
+
+            // Осі X і Y
+            g.DrawLine(axisPen, 0, cy, width, cy);
+            g.DrawLine(axisPen, cx, 0, cx, height);
+
+            // Стрілки
+            g.DrawLine(axisPen, width - 10, cy - 5, width, cy);
+            g.DrawLine(axisPen, width - 10, cy + 5, width, cy);
+            g.DrawLine(axisPen, cx - 5, 10, cx, 0);
+            g.DrawLine(axisPen, cx + 5, 10, cx, 0);
+
+            // Підписи осей
+            g.DrawString("X", font, brush, width - 15, cy + 5);
+            g.DrawString("Y", font, brush, cx + 5, 5);
+            g.DrawString("0", font, brush, cx + 3, cy + 3);
+
+            axisPen.Dispose();
+            font.Dispose();
+        }
+
+        // Визначає скільки повних обертів потрібно для замкнутої кривої
+        private int GetPeriods(double r, double R)
+        {
+            int ri = (int)Math.Round(r);
+            int Ri = (int)Math.Round(R);
+            if (ri == 0 || Ri == 0) return 1;
+
+            int gcd = GCD(ri, Ri);
+            return Ri / gcd;
+        }
+
+        private int GCD(int a, int b)
+        {
+            while (b != 0) { int t = b; b = a % b; a = t; }
+            return a;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double R = double.Parse(textBoxR.Text);
+                double r = double.Parse(textBox_r.Text);
+
+                if (R <= 0 || r <= 0)
+                    throw new ArgumentException("R і r повинні бути більше нуля.");
+
+                if (r >= R)
+                    throw new ArgumentException("r повинно бути менше R.");
+
+                DrawHypocycloid(R, r, pictureBox1);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введіть коректні числові значення.", "Помилка");
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, "Помилка");
+            }
+        }
+    }
+
+
+}
